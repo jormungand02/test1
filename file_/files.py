123e431df1fe32dd92d8c3d23b5f8493a35a6391
[Jormungand] <marlen.python@bk.ru>
2024-02-22 14:45:03 +0600

    r+ w+ a+

--- file_/files.py
+++ file_/files.py
@@ -55,3 +55,48 @@
 #     print(file.read())
 
 # print(file.closed) # True - файл закрылся False - открыт
+
+
+'РЕЖИМЫ'
+# r+
+# w+
+# a+
+
+'r+ = Открывает файл в режиме чтения + в режиме append(дозаписи)'
+# with open('test1.txt', 'r+') as file:
+#     print(file.read())
+#     file.write('py33')
+#     file.seek(0)
+#     print(file.read())
+
+
+'w+ = Открывает файлы в режиме записи + read(чтения) '
+# with open('test1.txt', 'w+') as file:
+#     file.write('qwerty')
+#     file.seek(0)
+#     text = file.read()
+#     print(text)
+
+
+'a+ = Открывает файл в режиме дозаписи + в режиме read(чтения)'
+# with open('test1.txt', 'a+') as file:
+#     file.read()
+#     file.write('makers\n')
+#     print(file.read())
+#     file.seek(0)
+#     file.write('hello\n')
+#     file.seek(0)
+#     print(file.read())
+
+'=======================CSV========================'
+# csv - формат файла 
+import csv
+
+# with open('data.csv') as file:
+#     data = csv.reader(file)
+#     colm = next(data)
+#     print('Поле', colm)
+#     for product in data:
+#         print(product)
+
+# writer статью посмотреть
